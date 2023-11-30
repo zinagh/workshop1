07bd20e72e8b30b57abff7e41dda5d1279525f0c
zinagh <zinaelislem.ghribi@esprit.tn>
2023-11-30 05:15:22 +0100

    updating imgs paths

--- src/app/components/residences/residences.component.ts
+++ src/app/components/residences/residences.component.ts
@@ -17,13 +17,13 @@ export class ResidencesComponent {
   showNotification: boolean = false;
   listResidences:Residence[]=[
   {id:1,"name": "El fel","address":"Borj Cedria",
-  "image":"../../assets/images/R1.jpeg"},
+  "image":"../../../assets/images/R1.jpeg"},
   {id:2,"name": "El yasmine",
-  "address":"Ezzahra","image":"../../assets/images/R2.jpg"},
+  "address":"Ezzahra","image":"../../../assets/images/R2.jpg"},
   {id:3,"name": "El Arij",
-  "address":"Rades","image":"../../assets/images/R3.jpg"},
+  "address":"Rades","image":"../../../assets/images/R3.jpg"},
   {id:4,"name": "El Anber","address":"Manzah 5",
-  "image":"../../assets/images/R4.jpg"}
+  "image":"../../../assets/images/R4.jpg"}
   ];
 
   listApartments:Apartment[]=[
